e0629856881807e94971d18f2d014b58757fe1bc
Andrey Tourkó <tourko@gmail.com>
2018-01-08 12:55:06 +0100

    Added detect() for multiline pattern matching.

---
+++ R/multiline.R
@@ -0,0 +1,78 @@
+multiline <- new.env()
+
+multiline$detect <- function(lines, patterns) {
+  # Apply N-lines pattern to the lines.
+  # Returns a list of N logical vectors - one for each pattern.
+  # Each vector has as may elements as there are lines.
+  # Elements in the vectors indicate which lines matched the pattern corresponding to the pattern.
+  #
+  # Example for 4-lines pattern and 53 lines:
+  # List of 4
+  #   $ pattern_1: int [1:53] 0 0 0 0 0 0 ...
+  #   $ pattern_2: int [1:53] 0 0 0 0 0 0 ...
+  #   $ pattern_3: int [1:53] 0 0 0 0 0 0 ...
+  #   $ pattern_4: int [1:53] 0 0 0 0 0 0 ...
+  l <- purrr::map(patterns, ~ lines %>% stringr::str_detect(pattern = .x) %>% as.integer())
+
+  # Convert the list into a matrix with N rows and as many columns as there are lines.
+  # Example:
+  #           [,1] [,2] [,3] [,4] [,5] [,6] [,7] [,8] [,9] [,10] [,11] [,12] ... [,53]
+  # pattern_1    0    0    0    0    0    0    0    0    1     0     0     0 ...     0
+  # pattern_2    0    0    0    0    0    0    0    0    0     1     0     0 ...     0
+  # pattern_3    0    0    0    0    0    0    0    0    0     0     1     0 ...     0
+  # pattern_4    0    0    0    0    0    0    0    0    0     0     0     1 ...     0
+  m <- matrix(purrr::flatten_int(l),
+              byrow = TRUE,
+              nrow = length(l),
+              dimnames = list(names(patterns)))
+
+  # Find indexes of the matrix with 1's
+  idx <- which(m == 1, arr.ind = TRUE)
+
+  # Replace 1's with the "col", which gives the line number
+  #           [,1] [,2] [,3] [,4] [,5] [,6] [,7] [,8] [,9] [,10] [,11] [,12] ... [,53]
+  # pattern_1    0    0    0    0    0    0    0    0    9     0     0     0 ...     0
+  # pattern_2    0    0    0    0    0    0    0    0    0    10     0     0 ...     0
+  # pattern_3    0    0    0    0    0    0    0    0    0     0    11     0 ...     0
+  # pattern_4    0    0    0    0    0    0    0    0    0     0     0    12 ...     0
+  for (i in seq_len(nrow(idx))) {
+    row <- idx[i, "row"]
+    col <- idx[i, "col"]
+    m[row, col] <- col
+  }
+
+  # A multiline pattern is detected, if there are M consequetive lines matching the patterns,
+  # where M is the number of patterns.
+  # Such lines will form a "diagonal" with 1's in the matrix.
+  # In the above example columns 10, 11, 12 and 13 form such "diagonal".
+  # In order to find the lines matching all M patterns, we shift the rows,
+  # so that 1's form a column.
+  # Example:
+  #            [,1] [,2] [,3] [,4] [,5] [,6] [,7] [,8] [,9] [,10] [,11] [,12] [,13] ... [,53]
+  # pattern_1     0    0    0    0    0    0    0    0    9     0     0     0    13 ...     0
+  # pattern_2     0    0    0    0    0    0    0    0   10     0     0     0    14 ...     0
+  # pattern_3     0    0    0    0    0    0    0    0   11     0     0     0    15 ...     0
+  # pattern_4     0    0    0    0    0    0    0    0   12     0     0     0    16 ...     0
+  for (i in seq_len(nrow(m))) {
+    # Skip the first row
+    if (i == 1) next
+    # Shift rows by n = i - 1 to the left, where "i" is the row number
+    m[i, ] <- data.table::shift(m[i, ], n = i - 1, fill = 0, type = "lead")
+  }
+
+  # Find columns that have at least one 0.
+  no_match <- which(colSums(m == 0) > 0)
+  # If there are any such columns, ...
+  if ( length(no_match) > 0 ) {
+    # ... drop them from the matrix
+    m <- m[, -which(colSums(m == 0) > 0), drop = FALSE]
+  }
+
+  # End result:
+  #           [,1] [,2]
+  # pattern_1    9   13
+  # pattern_2   10   14
+  # pattern_3   11   15
+  # pattern_4   12   16
+  return(m)
+}
---
+++ tests/testthat/test_multiline.R
@@ -0,0 +1,169 @@
+context("Multiline pattern matching")
+
+test_that("0 lines and empty pattern", {
+  lines <- c()
+
+  patterns <- c()
+
+  result   <- multiline$detect(lines, patterns)
+  expected <- matrix(integer(length(patterns)), dimnames = list(names(patterns)))[, -1, drop = FALSE]
+
+  expect_identical(result, expected)
+})
+
+test_that("0 lines and 1-line pattern", {
+  lines <- c()
+
+  patterns <- c(
+    pattern_1 = START %R% one_or_more(WRD %R% SPC) %R% "#1" %R% END
+  )
+
+  result   <- multiline$detect(lines, patterns)
+  expected <- matrix(integer(length(patterns)), dimnames = list(names(patterns)))[, -1, drop = FALSE]
+
+  expect_identical(result, expected)
+})
+
+test_that("1 line and an empty pattern", {
+  lines <- c("Test line #1")
+
+  patterns <- c()
+
+  result   <- multiline$detect(lines, patterns)
+  expected <- matrix(integer(length(patterns)), dimnames = list(names(patterns)))[, -1, drop = FALSE]
+
+  expect_identical(result, expected)
+})
+
+test_that("1 line and a matching 1-line pattern", {
+  lines <- c("Test line #1")
+
+  patterns <- c(
+    pattern_1 = START %R% one_or_more(WRD %R% SPC) %R% "#1" %R% END
+  )
+
+  result   <- multiline$detect(lines, patterns)
+  expected <- matrix(1L, nrow = length(patterns), dimnames = list(names(patterns)))
+
+  expect_identical(result, expected)
+})
+
+test_that("1 line and a non-matching 1-line pattern", {
+  lines <- c("Test line #1")
+
+  patterns <- c(
+    pattern_1 = START %R% one_or_more(WRD %R% SPC) %R% "#2" %R% END
+  )
+
+  result <- multiline$detect(lines, patterns)
+  expected <- matrix(integer(length(patterns)),  dimnames = list(names(patterns)))[, -1, drop = FALSE]
+
+  expect_identical(result, expected)
+})
+
+test_that("2 lines and a matching 2-lines pattern", {
+  lines <- c(
+    "Test line #1",
+    "Test line #2"
+  )
+
+  patterns <- c(
+    pattern_1 = START %R% one_or_more(WRD %R% SPC) %R% "#1" %R% END,
+    pattern_2 = START %R% one_or_more(WRD %R% SPC) %R% "#2" %R% END
+  )
+
+  result   <- multiline$detect(lines, patterns)
+  expected <- matrix(c(1L, 2L), nrow = length(patterns), dimnames = list(names(patterns)))
+
+  expect_identical(result, expected)
+})
+
+test_that("2 lines and a matching 1-line pattern", {
+  lines <- c(
+    "Test line #1",
+    "Test line #2"
+  )
+
+  patterns <- c(
+    pattern_1 = START %R% one_or_more(WRD %R% SPC) %R% "#" %R% DGT %R% END
+  )
+
+  result   <- multiline$detect(lines, patterns)
+  expected <- matrix(c(1L, 2L), nrow = length(patterns), dimnames = list(names(patterns)))
+
+  expect_identical(result, expected)
+})
+
+test_that("2 lines and a non-matching 1-line pattern", {
+  lines <- c(
+    "Test line #1",
+    "Test line #2"
+  )
+
+  patterns <- c(
+    pattern_1 = START %R% one_or_more(WRD %R% SPC) %R% "#" %R% END
+  )
+
+  result   <- multiline$detect(lines, patterns)
+  expected <- matrix(integer(length(patterns)), dimnames = list(names(patterns)))[, -1, drop = FALSE]
+
+  expect_identical(result, expected)
+})
+
+test_that("5 lines and a matching 2-lines pattern", {
+  lines <- c(
+    "Test line #1",
+    "Test line #2",
+    "Test line #3",
+    "Test line #1",
+    "Test line #2"
+  )
+
+  patterns <- c(
+    pattern_1 = START %R% one_or_more(WRD %R% SPC) %R% "#1" %R% END,
+    pattern_2 = START %R% one_or_more(WRD %R% SPC) %R% "#2" %R% END
+  )
+
+  result   <- multiline$detect(lines, patterns)
+  expected <- matrix(c(1L, 2L, 4L, 5L), nrow = length(patterns), dimnames = list(names(patterns)))
+
+  expect_identical(result, expected)
+})
+
+test_that("2 out of 4 lines matching 2-lines pattern", {
+  lines <- c(
+    "Test line #1",
+    "Test line #2",
+    "Test line #2",
+    "Test line #1"
+  )
+
+  patterns <- c(
+    pattern_1 = START %R% one_or_more(WRD %R% SPC) %R% "#1" %R% END,
+    pattern_2 = START %R% one_or_more(WRD %R% SPC) %R% "#2" %R% END
+  )
+
+  result   <- multiline$detect(lines, patterns)
+  expected <- matrix(c(1L, 2L), nrow = length(patterns), dimnames = list(names(patterns)))
+
+  expect_identical(result, expected)
+})
+
+test_that("2 out of 4 lines with overlapping matching 2-lines pattern", {
+  lines <- c(
+    "Test line #1",
+    "Test line #1",
+    "Test line #2",
+    "Test line #1"
+  )
+
+  patterns <- c(
+    pattern_1 = START %R% one_or_more(WRD %R% SPC) %R% "#1" %R% END,
+    pattern_2 = START %R% one_or_more(WRD %R% SPC) %R% "#2" %R% END
+  )
+
+  result   <- multiline$detect(lines, patterns)
+  expected <- matrix(c(2L, 3L), nrow = length(patterns), dimnames = list(names(patterns)))
+
+  expect_identical(result, expected)
+})
